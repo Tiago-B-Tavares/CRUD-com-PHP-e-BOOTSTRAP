4f77fa7ecbbadf65fcf2fab3117d0694094a272b
Tiago Tavares <tiagobritot2016@gmail.com>
2023-05-24 01:53:23 -0300

    reestruturando as sections da home

--- secoes/home2.php
+++ secoes/home2.php
@@ -1,19 +1,57 @@
-<section class="container-fluid row border background">
-    <article class="col-sm-12 col-md-8 border text-light   border">
-        <h1 class="secao-title text-center">God of War RagnaröK</h1>
-        <p class="w-50  px-5 ">
-            Os maiores heróis dos noce reinos, lutando por um futuro em que
+<section class="container-fluid row d-flex justify-content-center flex-row background">
+    <article class="col-sm-12 col-md-8  text-light">
+        <h1 class="secao-title text-left ps-5 ">God of War RagnaröK</h1>
+        <p class="w-50  p-4  fs-3 legenda">
+            Os maiores heróis dos nove reinos, lutando por um futuro em que
             acreditam
         </p>
     </article>
-    <article class="col-sm-12 col-md-4 border text-light ">
-        <figure>
-            <img src="src/img/thumbnail.gif" class="img-fluid text-center" alt="">
+    <article class="col-sm-12 col-md-4  text-light py-5  d-flex  flex-column">
+        <span>Confira o trailer</span>
+        <figure class="thumbnail text--left  d-flex justify-content-center">
+            <img src="src/img/thumbnail.gif" class=" img-fluid  border border-primary border-2" alt="">
         </figure>
-        <div class="w-50 border">
+        <div class="faixa-etaria">
             <img src="src/img/faixa-etaria.png" alt="">
-           <span class="w-25 border"> Drogas Licitas, linguagem Imprópia, Violência Extrema</span>
-</div>
+            <span class=" "> Drogas Licitas, linguagem Imprópia, Violência Extrema</span>
+        </div>
     </article>
-    
+
+</section>
+<section class="info-geral container-fluid row d-flex justify-content-center flex-row m-0 text-light">
+    <figure class="col-sm-6 col-md-2 info ">
+        <img src="src/img/icon-game-offline.png" alt="">
+        <span>Jogo offline habilitado</span>
+    </figure>
+    <figure class="col-sm-6 col-md-2 info ">
+        <img src="src/img/icon-singleplayer.png" alt="">
+        <span>1 Jogador </span>
+    </figure>
+    <figure class="col-sm-6 col-md-2 info ">
+        <img src="src/img/icon-use-remote.png" alt="">
+        <span>Compátivel com uso remote</span>
+    </figure>
+    <figure class="col-sm-6 col-md-2 info ">
+        <img src="src/img/icon-function-vibration.png" alt="">
+        <span>Função de vibração necessária</span>
+    </figure>
+    <figure class="col-sm-6 col-md-2 info ">
+        <img src="src/img/icon-effect.png" alt="">
+        <span>Efeito gatilho necessário</span>
+    </figure>
+    <figure class="col-sm-6 col-md-2 info ">
+        <img src="src/img/icon-psn-plus.png" alt="">
+        <span>Compatível com a ajuda do jogo</span>
+    </figure>
+
+</section>
+<section class="row">
+    <article class="col-6 text-light">
+        <span>Sony Interactive Entertainment</span>
+        <h2>Embarque com Kratos e Atreus em uma jornada épica</h2>
+        O Fimbulwinter já começou. Kratos e Atreus devem viajar pelos Nove Reinos em busca de respostas enquanto as forças asgardianas se preparam para uma batalha profetizada que causará o fim do mundo. Nessa jornada, eles explorarão paisagens míticas impressionantes e enfrentarão inimigos aterradores: deuses nórdicos e monstros. A ameaça do Ragnarök se aproxima. Kratos e Atreus terão de escolher entre a segurança deles próprios e a dos reinos.
+    </article>
+    <figure class="col-6">
+        <img src="src/img/kratos-atreus.png" alt="">
+    </figure>
 </section>
